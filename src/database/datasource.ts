7bab9c5c3edbbddc531ce2a95fc67f2e7289c909
FrancieliSauthier <francieli.sauthier@gmail.com>
2024-11-19 17:16:06 +0100

    add database configuration

---
+++ src/database/datasource.ts
@@ -0,0 +1,24 @@
+import 'reflect-metadata';
+import { DataSource } from 'typeorm';
+import { configs } from './../config/env.js';
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: configs.db.POSTGRES_HOST,
+  port: configs.db.POSTGRES_PORT,
+  username: configs.db.POSTGRES_USER,
+  password: configs.db.POSTGRES_PASSWORD,
+  database: configs.db.POSTGRES_DB,
+  // entities
+  synchronize: true,
+  logging: true,
+});
+
+export const connectDB = async () => {
+  try {
+    await AppDataSource.initialize();
+    console.log('Connected to database');
+  } catch (error) {
+    console.log('Error in connecting to database', error);
+  }
+};
