c4ecc2f8e096bc026de272e9980c1087478e88c0
FrancieliSauthier <francieli.sauthier@gmail.com>
2024-11-19 17:17:38 +0100

    add general configuration  and changes

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import express from 'express'
-import {configs} from './configs/env.js'
+import express from 'express';
+import { configs } from './config/env.js';
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Server working')
-})
+  res.send('Server working');
+});
 
 app.listen(configs.PORT, () => {
-  console.log(`Server running on http://localhost:${configs.PORT}`);
-})
+  console.log(`Server bbb running on http://localhost:${configs.PORT}`);
+});
