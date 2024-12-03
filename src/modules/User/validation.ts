67439b51ee6b98c5678bea40ab43af9be3e777a5
FrancieliSauthier <francieli.sauthier@gmail.com>
2024-12-03 20:09:50 +0100

    refactor schema sintaxe to the body

--- src/modules/User/validation.ts
+++ src/modules/User/validation.ts
@@ -1,14 +1,18 @@
 import Joi from 'joi';
 
-const registerSchema = Joi.object({
-  email: Joi.string().email().required(),
-  password: Joi.string().min(6).required(),
-  name: Joi.string().required(),
-});
+const registerSchema = {
+  body: Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).required(),
+    name: Joi.string().required(),
+  }),
+};
 
-const loginSchema = Joi.object({
-  email: Joi.string().email().required(),
-  password: Joi.string().min(6).required(),
-});
+const loginSchema = {
+  body: Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).required(),
+  }),
+};
 
 export default { registerSchema, loginSchema };
